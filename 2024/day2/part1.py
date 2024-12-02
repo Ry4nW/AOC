8e8be9184b5c065ed7ee65d1ead6c6b2c5e6b967
Ryan <ryanbcwang@gmail.com>
2024-12-01 23:48:56 -0800

    d2

---
+++ 2024/day2/part1.py
@@ -0,0 +1,19 @@
+data = [x.rstrip() for x in open("input.txt").readlines()]
+
+def safe(l):
+  sc = sorted(l)
+  rsc = sc.copy()
+  rsc.reverse()
+  if not (sc == l or rsc == l): return False
+  for i in range(1, len(l)):
+    dif = abs(l[i] - l[i - 1])
+    if not (1 <= dif and dif <= 3): return False
+  return True
+
+r = 0
+for l in data:
+  c = list(map(int, l.split(' ')))
+  for i in range(len(c)):
+    if safe(c): r += 1; break
+      
+print(r)
